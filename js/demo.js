2856778c7ead71b7dbfa8dd825a9c6a922fee3b0
Gael CORDON <cordon.gael@gmail.com>
2017-01-16 16:44:44 +0100

    etape 16h44

--- js/demo.js
+++ js/demo.js
@@ -1,1 +1,28 @@
 console.log("Console is ok !");
+
+var jsdevFront = document.getElementById("devFront");
+var html5 = document.getElementById('html5');
+var css3 = document.getElementById('css3');
+var javascript = document.getElementById('javascript');
+var devback = document.getElementById('devback');
+var php = document.getElementById('php');
+var mysql = document.getElementById('mysql');
+var frameworks = document.getElementById('frameworks');
+
+var tableau = [jsdevFront,html5,css3,javascript,devback,php,mysql,frameworks];
+
+for (i=0; i<tableau.length; i++ ){
+tableau[i].style.visibility="hidden";
+
+}
+
+document.getElementById('submit').onclick= function(){
+
+	// console.log('bien cliqué');
+	for (i=0; i<tableau.length; i++ ){
+	var question = prompt("voulez-vous continuer ?");
+	if (question === "oui"){
+	tableau[i].style.visibility="visible";
+	} else;
+	};
+};
